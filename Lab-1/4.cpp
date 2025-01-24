3d289c88ae50d335b1a1f3b4dac4e795d497c0e2
Syedsaad40 <mosyedsaad11@gmail.com>
2025-01-24 10:04:53 +0500

    Add files via upload

---
+++ Lab-1/4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    float kg, pound;
+    cout << "Enter the value in kg: ";  
+    cin >> kg;  
+    pound = 2.2 * kg;  
+    cout << "The value in pounds is " << pound;  
+    return 0;
+}
